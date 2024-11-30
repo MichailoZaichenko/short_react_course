cdec05ac867193d573f3a135515f30866b457bff
MichailoZaichenko <michailo.zaichenko@gmail.com>
2024-11-30 17:48:09 +0200

    Props and hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name} </h1>
+    <h2>Last Name: {props.lastname}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'null';
+  const isNameShowing = true;
+  // hook
+  const [counter, setCounter] = useState(0);
+  // hook #2
+  useEffect(() => {
+    alert('You changed the counter to '+counter)
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello, </h1>
+        {name ? (
+          <>
+            <h1> {name}</h1>
+          </>
+        ): (
+          <>
+          <h1>test</h1>
+          <h2> There is no name</h2>
+          </>
+        )}
+      <Person />
+      {/* Props */}
+      <Person 
+        name={'Misha'} 
+        lastname={'Zaichenko'} 
+        age={'18'}/>
+      {/* If string we can use just '' */}
+      <Person name={'Jane'} lastname='Doe' age={2+2}/>
+      <button onClick = {() => setCounter((prevCount) => prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick = {() => setCounter((prevCount) => prevCount +1)}>+</button>
+      
     </div>
   );
 }
